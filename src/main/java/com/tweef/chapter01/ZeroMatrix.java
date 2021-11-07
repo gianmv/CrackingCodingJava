6338484737f9a78e5c5d2c5d28099285c96dc2ed
Emmanuel <emrz98@gmail.com>
2021-11-06 18:20:35 -0600

    zero matrix

--- src/main/java/com/tweef/chapter01/ZeroMatrix.java
+++ src/main/java/com/tweef/chapter01/ZeroMatrix.java
@@ -22,18 +22,42 @@ public class ZeroMatrix {
         for (int i = 0; i < num_rows; i++) {
             for (int j = 0; j < num_columns; j++) {
                 if (zeroMatrix[i][j] == 0){
-                    info_matrix.add(new Integer[]{i, j});
+                    zeroMatrix[i][0] = 0;
+                    zeroMatrix[0][j] = 0;
                 }
             }
         }
-        for (Integer[] element: info_matrix) {
+        printMatrix(zeroMatrix);
+        for (int i = num_columns-1; i > 0; i--) {
+            if (zeroMatrix[0][i] == 0){
+                for (int j = 0; j < num_rows; j++) {
+                    zeroMatrix[j][i] = 0;
+                }
+            }
+        }
+        for (int i = num_rows - 1; i > 0; i--) {
+            if (zeroMatrix[i][0] == 0){
+                for (int j = 0; j < num_columns; j++) {
+                    zeroMatrix[i][j] = 0;
+                }
+            }
+        }
+        if (zeroMatrix[0][0] == 0){
+            for (int j = 0; j < num_columns; j++) {
+                zeroMatrix[0][j] = 0;
+            }
+            for (int j = 0; j < num_rows; j++) {
+                zeroMatrix[j][0] = 0;
+            }
+        }
+        /*for (Integer[] element: info_matrix) {
             for (int i = 0; i < num_rows; i++) {
                 zeroMatrix[i][element[1]] = 0;
             }
             for (int i = 0; i < num_columns; i++) {
                 zeroMatrix[element[0]][i] = 0;
             }
-        }
+        }*/
         printMatrix(zeroMatrix);
         return zeroMatrix;
     }
